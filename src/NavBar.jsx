9ffc8cf0e0eb274a8511f91fb80bec29e1ebf558
Ignacio Arenas <86266802+IgnacioArenas@users.noreply.github.com>
2022-11-29 20:01:58 -0300

    Add files via upload

---
+++ src/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = ({campeones, runas, objetos}) => {
+    return (
+        <div style={{backgroundColor: 'darkred'}}>
+            {campeones}, {runas}, {objetos}
+        </div>
+    )
+}
+
+export default NavBar;
